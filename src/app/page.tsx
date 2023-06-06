185d8f25b1f994353b3bf8e56ba8ee2f0ee6a365
mambatukaa <sbatuka9@gmail.com>
2023-06-06 10:32:34 -0700

    add footer on page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Header from "@/components/Header";
 import About from "@/components/About";
 import Leaders from "@/components/Leaders";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Header />
       <About />
       {/* <Leaders /> */}
+      <Footer />
     </main>
   );
 }
