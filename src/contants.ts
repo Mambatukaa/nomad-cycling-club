1bb671cf447490833e7b649c033dae439021772b
mambatukaa <sbatuka9@gmail.com>
2023-06-08 10:34:06 -0700

    add contants and types

---
+++ src/contants.ts
@@ -0,0 +1,40 @@
+import { ILeader } from "./types";
+
+export const LEADERS: ILeader[] = [
+  {
+    id: 1,
+    fullName: "Batbayar Batchuluun",
+    avatar: "https://avatars.githubusercontent.com/u/11501555?v=4",
+    role: "Team Captain",
+    summary:
+      "Originally from AZ, Bagi has infiltrated the Nomads quick and now serving as a Captain of the Club as well as US Mongolian Cycling Association. He is a strong rider and a great leader.",
+    socialAccounts: [
+      {
+        name: "facebook",
+        url: "https://www.facebook.com/batbayar.batchuluun",
+      },
+      {
+        name: "twitter",
+        url: "https://www.twitter.com/batbayar.batchuluun",
+      },
+    ],
+  },
+  {
+    id: 2,
+    fullName: "Boldbaatar Batchuluun",
+    avatar: "https://avatars.githubusercontent.com/u/11501555?v=4",
+    role: "Manager",
+    summary:
+      "Jorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+    socialAccounts: [
+      {
+        name: "facebook",
+        url: "https://www.facebook.com/boldbaatar.batchuluun",
+      },
+      {
+        name: "twitter",
+        url: "https://www.twitter.com/boldbaatar.batchuluun",
+      },
+    ],
+  },
+];
---
+++ src/types.ts
@@ -0,0 +1,13 @@
+export interface ISocialAccount {
+  name: string;
+  url: string;
+}
+
+export interface ILeader {
+  id: number;
+  fullName: string;
+  summary: string;
+  avatar: string;
+  role: string;
+  socialAccounts: ISocialAccount[];
+}
