bdc943d0fb0088573b9099fc06b081ac1e8f8e73
mambatukaa <sbatuka9@gmail.com>
2023-07-01 13:00:34 -0700

    add shadow on the background image and update text's line height

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -13,16 +13,17 @@ const About: React.FC = () => {
           fill={true}
           objectFit="cover"
         />
+        <div className="absolute inset-0 bg-gradient-to-l from-black opacity-40 "></div>
       </div>
 
       <div className="relative mx-auto md:grid grid-cols-3 self-center md:container max-md:px-5">
 
         <div className="col-end-4">
-          <h1 className="text-5xl font-bold text-yellow">
+          <h1 className="text-5xl font-bold text-yellow leading-tight">
             We are a team of Mongolian bike riders
           </h1>
 
-          <p className="mt-4 text-2xl text-white">
+          <p className="mt-4 text-2xl text-white font-light leading-snug">
             Our club is dedicated to providing a welcoming and supportive
             environment for all cyclists, regardless of their skill level or
             backgroud. We offer a range of rides and events that cater to
