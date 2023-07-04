d8ee4d14decfaaf4aa942f959f195f96c5f58121
mambatukaa <sbatuka9@gmail.com>
2023-07-04 07:54:59 -0700

    add section and improve responsive and sizing css

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -10,38 +10,44 @@ import "yet-another-react-lightbox/styles.css";
 import "yet-another-react-lightbox/plugins/thumbnails.css";
 import "yet-another-react-lightbox/plugins/counter.css";
 import GalleryCard from "./common/GalleryCard";
+import Section from "./common/Section";
 
 const Gallery = () => {
   const [open, setOpen] = React.useState(false);
 
   const [slides, setSlides] = React.useState([
-    { src: "/assets/images/gallery/image1.jpeg" },
-    { src: "/assets/images/gallery/image2.jpeg" },
-    { src: "/assets/images/gallery/image3.jpeg" },
+    { src: "/assets/images/gallery/covers/image1.jpeg" },
+    { src: "/assets/images/gallery/covers/image2.jpeg" },
+    { src: "/assets/images/gallery/covers/image3.jpeg" },
   ]);
 
   return (
-    <div id="albums" className="flex bg-gray-100 py-24">
-      <div className="container mx-auto">
-        <div className="max-md:px-5">
-          <h1 className="text-5xl font-bold text-blue-primary">Photo Albums</h1>
-          <p className="mt-4 text-lg text-black md:w-[485px]">
+    <div id="albums" className="flex bg-gray-100">
+      <Section
+        title="Photo Albums"
+        description={`
             Our club is dedicated to providing a welcoming and supportive
             environment for all cyclists, regardless of their skill level or
             backgroud.
-          </p>
+        
+        `}
+      >
+        <div className="mb-1 grid lg:grid-cols-2 gap-1">
+          <GalleryCard onClick={() => setOpen(true)} title="2021 Competition" imgCode={1} />
+          <GalleryCard onClick={() => setOpen(true)} title="2022 Competition" imgCode={2} />
         </div>
 
-        <div className="my-14 grid h-96 grid-cols-2">
-          <div className="relative pr-1" onClick={() => setOpen(true)}>
-            <GalleryCard title="2021 Competition" />
-          </div>
-
-          <div className="relative pl-1">
-            <GalleryCard title="2022 Competition" />
-          </div>
+        <div className="mb-1 grid lg:grid-cols-3 gap-1">
+          <GalleryCard onClick={() => setOpen(true)} title="2021 Competition" imgCode={3} />
+          <GalleryCard onClick={() => setOpen(true)} title="2022 Competition" imgCode={4} />
+          <GalleryCard onClick={() => setOpen(true)} title="2022 Competition" imgCode={5} />
         </div>
-      </div>
+
+        <div className="mb-1 grid lg:grid-cols-2 gap-1">
+          <GalleryCard onClick={() => setOpen(true)} title="2021 Competition" imgCode={7} />
+          <GalleryCard onClick={() => setOpen(true)} title="2021 Competition" imgCode={8} />
+        </div>
+      </Section>
 
       <Lightbox
         open={open}
--- src/components/common/GalleryCard.tsx
+++ src/components/common/GalleryCard.tsx
@@ -4,32 +4,40 @@ import { ReactElement, FC } from "react";
 interface IProps {
   title: string;
   description?: string;
+  imgCode: number;
+  onClick: () => void;
 }
 
-const GalleryCard: FC<IProps> = ({ title, description }): ReactElement => {
+const GalleryCard: FC<IProps> = ({
+  title,
+  description,
+  imgCode,
+  onClick
+}): ReactElement => {
   return (
-    <div className="pt-3">
+    <div onClick={() => onClick()}>
       <div className="group relative aspect-video cursor-pointer overflow-hidden">
         <div className="absolute inset-x-0 -bottom-2 z-50 flex cursor-pointer items-end rounded-xl bg-gradient-to-t from-black/90 to-transparent opacity-0 transition duration-300 ease-in-out group-hover:opacity-100">
           <div>
-            <div className="translate-y-4 transform space-y-3 p-4 pb-10 text-xl transition duration-300 ease-in-out group-hover:translate-y-0 group-hover:opacity-100">
-              <div className="font-bold ">{title}</div>
+            <div className="translate-y-4 transform space-y-3 p-4 transition duration-300 ease-in-out group-hover:translate-y-0 group-hover:opacity-100 md:pb-10 md:text-xl">
+              <div className="md:text-md font-bold">{title}</div>
 
-              <div className="text-sm opacity-90 ">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Distinctio dolores error iure, perferendis sequi totam. Ad
-                aliquam aperiam atque deleniti dolor dolorem enim esse et in,
-                inventore itaque, pariatur reprehenderit.
+              <div className="opacity-90 md:text-sm">
+                {description ||
+                  `Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Distinctio dolores error iure, perferendis sequi totam.`}{" "}
               </div>
             </div>
           </div>
         </div>
+
         <Image
-          src="/assets/images/gallery/image1.jpeg"
+          src={`/assets/images/gallery/covers/image${imgCode}.jpeg`}
           fill={true}
           objectFit="cover"
           alt="gallery"
           className="aspect-square w-full object-cover transition duration-300 ease-in-out group-hover:scale-110"
+          quality={100}
         />
       </div>
     </div>
