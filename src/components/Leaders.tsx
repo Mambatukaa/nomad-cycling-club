63508b879d45014d7047aa6f83ac15efad8e2292
mambatukaa <sbatuka9@gmail.com>
2023-07-04 07:53:29 -0700

    add common component named section

--- src/components/Leaders.tsx
+++ src/components/Leaders.tsx
@@ -7,53 +7,49 @@ import "swiper/css";
 import "swiper/css/pagination";
 import { Pagination } from "swiper";
 
+import Section from "./common/Section";
+
 const Leaders = () => {
   return (
-    <div id="leaders" className="flex bg-gray-200 py-24">
-      <div className="container mx-auto">
-        <div className="max-lg:px-5">
-          <h1 className="text-5xl font-bold text-blue-primary">
-            Nomads Leadership
-          </h1>
-
-          <p className="mt-4 text-lg text-black md:w-[485px]">
+    <div id="leaders" className="flex bg-gray-200">
+      <Section
+        title="Nomads Leadership"
+        description={`
             Our club is dedicated to providing a welcoming and supportive
             environment for all cyclists, regardless of their skill level or
             backgroud.
-          </p>
-        </div>
-
-        <div className="my-14 max-lg:px-5">
-          <Swiper
-            slidesPerView={1}
-            spaceBetween={10}
-            pagination={{
-              clickable: true,
-            }}
-            modules={[Pagination]}
-            breakpoints={{
-              640: {
-                slidesPerView: 1,
-                spaceBetween: 20,
-              },
-              768: {
-                slidesPerView: 2,
-                spaceBetween: 20,
-              },
-              1024: {
-                slidesPerView: 3,
-                spaceBetween: 20,
-              },
-            }}
-          >
-            {LEADERS.map((leader, index) => (
-              <SwiperSlide key={leader.id} virtualIndex={index}>
-                <LeaderCard key={leader.id} leader={leader} />
-              </SwiperSlide>
-            ))}
-          </Swiper>
-        </div>
-      </div>
+        
+        `}
+      >
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={10}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Pagination]}
+          breakpoints={{
+            640: {
+              slidesPerView: 1,
+              spaceBetween: 20,
+            },
+            768: {
+              slidesPerView: 2,
+              spaceBetween: 20,
+            },
+            1024: {
+              slidesPerView: 3,
+              spaceBetween: 20,
+            },
+          }}
+        >
+          {LEADERS.map((leader, index) => (
+            <SwiperSlide key={leader.id} virtualIndex={index}>
+              <LeaderCard key={leader.id} leader={leader} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </Section>
     </div>
   );
 };
---
+++ src/components/common/Section.tsx
@@ -0,0 +1,26 @@
+import { ReactNode, ReactElement, FC } from "react";
+
+interface IProps {
+  title: string;
+  description: string;
+  children: ReactNode;
+}
+
+const Section: FC<IProps> = ({
+  children,
+  title,
+  description,
+}): ReactElement => {
+  return (
+    <div className="container mx-auto">
+      <div className="my-14 max-lg:px-5">
+        <h1 className="text-5xl font-bold text-blue-primary">{title}</h1>
+        <p className="mt-4 text-lg text-black md:w-[485px]">{description}</p>
+      </div>
+
+      <div className="mb-14 max-lg:px-5">{children}</div>
+    </div>
+  );
+};
+
+export default Section;
