82b29f7e30f0315164f38b8c74a02d0bf73df84a
mambatukaa <sbatuka9@gmail.com>
2023-07-09 20:09:56 -0700

    add svgr that svg into react component library

--- next.config.js
+++ next.config.js
@@ -12,6 +12,15 @@ const nextConfig = {
       },
     ],
   },
+
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [{ loader: "@svgr/webpack", options: { icon: true } }],
+    });
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
