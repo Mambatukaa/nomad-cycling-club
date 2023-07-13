6ab428fce8de0671a0f6f6933118341f48090c42
mambatukaa <sbatuka9@gmail.com>
2023-07-13 11:53:49 -0700

    move image gallery swiper

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,28 +1,10 @@
-"use client";
-import Image from "next/image";
 import * as React from "react";
-import Lightbox from "yet-another-react-lightbox";
-import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
-import Zoom from "yet-another-react-lightbox/plugins/zoom";
-import Counter from "yet-another-react-lightbox/plugins/counter";
-
-import "yet-another-react-lightbox/styles.css";
-import "yet-another-react-lightbox/plugins/thumbnails.css";
-import "yet-another-react-lightbox/plugins/counter.css";
 import GalleryCard from "./common/GalleryCard";
 import Section from "./common/Section";
 
 const Gallery = () => {
-  const [open, setOpen] = React.useState(false);
-
-  const [slides, setSlides] = React.useState([
-    { src: "/assets/images/gallery/covers/image1.jpeg" },
-    { src: "/assets/images/gallery/covers/image2.jpeg" },
-    { src: "/assets/images/gallery/covers/image3.jpeg" },
-  ]);
-
   return (
-    <div id="albums" className="flex bg-gray-100 scroll-mt-16">
+    <div id="albums" className="flex scroll-mt-16 bg-gray-100">
       <Section
         title="Photo Albums"
         description={`
@@ -32,30 +14,22 @@ const Gallery = () => {
         
         `}
       >
-        <div className="mb-1 grid lg:grid-cols-2 gap-1">
-          <GalleryCard onClick={() => setOpen(true)} title="2021 Competition" imgCode={1} />
-          <GalleryCard onClick={() => setOpen(true)} title="2022 Competition" imgCode={2} />
+        <div className="mb-1 grid gap-1 lg:grid-cols-2">
+          <GalleryCard title="2021 Competition" imgCode={1} />
+          <GalleryCard title="2022 Competition" imgCode={2} />
         </div>
 
-        <div className="mb-1 grid lg:grid-cols-3 gap-1">
-          <GalleryCard onClick={() => setOpen(true)} title="2021 Competition" imgCode={3} />
-          <GalleryCard onClick={() => setOpen(true)} title="2022 Competition" imgCode={4} />
-          <GalleryCard onClick={() => setOpen(true)} title="2022 Competition" imgCode={5} />
+        <div className="mb-1 grid gap-1 lg:grid-cols-3">
+          <GalleryCard title="2021 Competition" imgCode={3} />
+          <GalleryCard title="2022 Competition" imgCode={4} />
+          <GalleryCard title="2022 Competition" imgCode={5} />
         </div>
 
-        <div className="mb-1 grid lg:grid-cols-2 gap-1">
-          <GalleryCard onClick={() => setOpen(true)} title="2021 Competition" imgCode={7} />
-          <GalleryCard onClick={() => setOpen(true)} title="2021 Competition" imgCode={8} />
+        <div className="mb-1 grid gap-1 lg:grid-cols-2">
+          <GalleryCard title="2021 Competition" imgCode={7} />
+          <GalleryCard title="2021 Competition" imgCode={8} />
         </div>
       </Section>
-
-      <Lightbox
-        open={open}
-        counter={{ container: { style: { top: "unset", bottom: 0 } } }}
-        plugins={[Thumbnails, Zoom, Counter]}
-        close={() => setOpen(false)}
-        slides={slides}
-      />
     </div>
   );
 };
--- src/components/common/GalleryCard.tsx
+++ src/components/common/GalleryCard.tsx
@@ -1,21 +1,38 @@
+"use client";
 import Image from "next/image";
-import { ReactElement, FC } from "react";
+import { ReactElement, FC, useState } from "react";
+
+// import lightbox styles
+import "yet-another-react-lightbox/styles.css";
+import "yet-another-react-lightbox/plugins/thumbnails.css";
+import "yet-another-react-lightbox/plugins/counter.css";
+
+import Lightbox from "yet-another-react-lightbox";
+import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
+import Zoom from "yet-another-react-lightbox/plugins/zoom";
+import Counter from "yet-another-react-lightbox/plugins/counter";
 
 interface IProps {
   title: string;
   description?: string;
   imgCode: number;
-  onClick: () => void;
 }
 
 const GalleryCard: FC<IProps> = ({
   title,
   description,
   imgCode,
-  onClick
 }): ReactElement => {
+  const [open, setOpen] = useState(false);
+
+  const [slides, setSlides] = useState([
+    { src: "/assets/images/gallery/covers/image1.jpeg" },
+    { src: "/assets/images/gallery/covers/image2.jpeg" },
+    { src: "/assets/images/gallery/covers/image3.jpeg" },
+  ]);
+
   return (
-    <div onClick={() => onClick()}>
+    <div onClick={() => setOpen(true)}>
       <div className="group relative aspect-video cursor-pointer overflow-hidden">
         <div className="absolute inset-x-0 -bottom-2 z-50 flex cursor-pointer items-end rounded-xl bg-gradient-to-t from-black/90 to-transparent opacity-0 transition duration-300 ease-in-out group-hover:opacity-100">
           <div>
@@ -40,6 +57,14 @@ const GalleryCard: FC<IProps> = ({
           quality={100}
         />
       </div>
+
+      <Lightbox
+        open={open}
+        counter={{ container: { style: { top: "unset", bottom: 0 } } }}
+        plugins={[Thumbnails, Zoom, Counter]}
+        close={() => setOpen(false)}
+        slides={slides}
+      />
     </div>
   );
 };
