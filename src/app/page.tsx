132237899984d756924b48379035e53ee76fb2c6
mambatukaa <sbatuka9@gmail.com>
2023-07-30 17:36:03 -0700

    little update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,13 @@
-import Header from '@/components/Header';
 import About from '@/components/About';
 import Leaders from '@/components/Leaders';
-import Footer from '@/components/Footer';
 import Gallery from '@/components/Gallery';
 
 export default function Home() {
   return (
     <main>
-      <Header />
       <About />
       <Leaders />
       <Gallery />
-      <Footer />
     </main>
   );
 }
