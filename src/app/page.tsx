0cd762d4a4fa98ef5584a7bcef30e80352e12af0
mambatukaa <sbatuka9@gmail.com>
2024-05-13 22:52:05 -0700

    show challenge sway in i-frame under header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import About from '@/components/About';
 import Leaders from '@/components/Leaders';
 import Gallery from '@/components/Gallery';
+import Challenge from '@/components/Challenge';
 
 export default function Home() {
   return (
     <main>
+      <Challenge />
       <About />
       <Leaders />
       <Gallery />
---
+++ src/components/Challenge.tsx
@@ -0,0 +1,12 @@
+const Challenge: React.FC = () => {
+  return (
+    <div>
+      <iframe
+        className="aspect-[4/10] w-full"
+        src="https://sway.cloud.microsoft/s/WrM6W2SYt01arQzT/embed"
+      ></iframe>
+    </div>
+  );
+};
+
+export default Challenge;
